69b382c4bed7605d0505c5cf191c5c20f256809c
Reuvin Hernandez <reuvinhernandez22@gmail.com>
2024-05-06 11:04:38 +0800

    Update

--- src/pages/AdminPages/Dashboard.jsx
+++ src/pages/AdminPages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Cards from '../../components/Cards'
-import CanvasJSReact from '@canvasjs/react-charts';
+import CanvasJSReact from '../../canvasjs.react';
 
 function Dashboard() {
     var CanvasJSChart = CanvasJSReact.CanvasJSChart;
